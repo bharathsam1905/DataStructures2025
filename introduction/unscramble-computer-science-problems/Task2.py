8197b6747a93b88e180f35f932e62c6151634084
bharathsam1905 <bharath.sam1905@gmail.com>
2024-11-05 20:43:29 -0500

    Commit module0

--- introduction/unscramble-computer-science-problems/Task2.py
+++ introduction/unscramble-computer-science-problems/Task2.py
@@ -2,6 +2,7 @@
 Read file into texts and calls.
 It's ok if you don't understand how to read files
 """
+from datetime import datetime
 import csv
 with open('texts.csv', 'r') as f:
     reader = csv.reader(f)
@@ -19,4 +20,22 @@ Print a message:
 "<telephone number> spent the longest time, <total time> seconds, on the phone during 
 September 2016.".
 """
+print('values',max(calls))
+#print('dateiem',datetime.strptime(calls[0][2],"%m-%d-%Y %H:%M:%S").date())
+call_dict={}
+for i in range(len(calls)):
+    startdate=datetime(2016,9,1).date()  
+    enddate=datetime(2016,9,30).date()
+    inputdate=datetime.strptime(calls[i][2],"%d-%m-%Y %H:%M:%S").date()
+  
+  #print('startdate',startdate)
+  #print('inputdate',inputdate)
+    if(inputdate>=startdate and inputdate<=enddate):  
+     key=calls[i][0]
+     value=calls[i][3]    
+     call_dict[key]=int(value)
 
+print('call_dict',call_dict)
+max_key, max_value = max(call_dict.items(), key=lambda x: x[1])
+print('Telephone Number',max_key,'spent the longest time,',max_value,'seconds, on the phone during \
+September 2016.')
