569ecbff1f0d6bb1b86bd36e9960f1bf07d38fe5
Bharath Samaved <bsamavedam@follett.com>
2026-01-22 15:46:51 -0500

    Implement dequeue method and enhance queue testing

--- NeetCode/Static Arrays/Queues/ImplementQueue.py
+++ NeetCode/Static Arrays/Queues/ImplementQueue.py
@@ -18,14 +18,35 @@ class Queue:
             self.right=newNode
             self.left=self.right
     
+    def dequeue(self):
+        if not self.left:
+            return None
+        val=self.left.val
+        self.left=self.left.next
+        if not self.left:
+            self.right=None
+        return val
+
+    
     def print(self):
         curr=self.left
         while curr:
             print(curr.val,'->',end=' ')
             curr=curr.next
+        print()
 
 q=Queue()        
 q.enqueue(5)
+q.enqueue(8)
+q.enqueue(9)
+q.print()
+q.dequeue()
+q.print()
+q.dequeue()
+q.print()
+q.dequeue()
+q.print()
+q.dequeue()
 q.print()
             
  
